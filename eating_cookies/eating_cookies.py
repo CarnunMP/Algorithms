62c30a0d67226270d7eae0d4bebb640638c78eb6
CarnunMP <carnun@hotmail.co.uk>
2020-02-20 09:09:05 +0000

    Eating cookies with memoization done.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -15,7 +15,15 @@ def eating_cookies(n, cache=None):
     if n == 3:
         return 4
 
-    return eating_cookies(n - 1) + eating_cookies (n - 2) + eating_cookies(n - 3)
+    if cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            cache = {i:0 for i in range(n + 1)}
+        
+        cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+
+        return cache[n]
 
 ### First attempt:
 # def eating_cookies(n, cache=None):
